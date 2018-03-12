21141b879b048cd411548a55d97816ce2ef6cd7e
Terry Churchill <tchurchill@datapipe.com>
2018-03-12 08:18:13 +0000

    Update

--- furl.rb
+++ furl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Furl < Formula
   homepage 'https://github.com/bingos/furl/tarball/master'
-  url 'file:///Users/terry/work/furl-2.1.tar.gz'
-  sha1 'ed27475de3917dcc9a61b7c82de4dfebd949e571'
+  url 'file:///Users/tchurchill/work/furl-2.1.tar.gz'
+  sha256 '6ca6138c4afedacdb192e3d06f767d1679715694f377c65ca2a230fe111bb1e3'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
