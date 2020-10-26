47c2eea6b35c84e392d7bb114116af72ea52ab79
Terry Churchill <terry.churchill@e5580-2trb5m2.uk.access-accounts.com>
2020-10-26 10:58:03 +0000

    Move to same dir.

--- furl.rb
+++ furl.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Furl < Formula
   homepage 'https://github.com/bingos/furl/tarball/master'
-  url 'file:///Users/tchurchill/work/furl-2.1.tar.gz'
+  url 'file:///Users/terry.churchill/Documents/GitHub/brew-formulas/furl-2.1.tar.gz'
   sha256 '6ca6138c4afedacdb192e3d06f767d1679715694f377c65ca2a230fe111bb1e3'
 
   def install
